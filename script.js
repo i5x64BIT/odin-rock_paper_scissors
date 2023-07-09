e43dde22412006c7dd834f5a78c4e1fe9d0e0ffd
i5x64BIT <vaintomer@gmail.com>
2023-07-09 17:46:11 +0300

    Modify game logic to fit UI
    Inputs are represented as numbers in the UI.

--- script.js
+++ script.js
@@ -2,34 +2,56 @@ const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
-function game (){
-    //rework the logic
+let playerInput;
+let computerInput;
 
-    // console.log('Script is running')
-    // for(let i = 0; i<=5 ; i++){
-    //     input = getPlayerInput()
-    //     if(input){
-    //         descision(input, getComputerInput());
-    //     }
-    // }
+let playerWins = 0;
+let computerWins = 0;
+
+function game (){
+    console.log('Game has Started!')
+    
+
+    initGame();
 }
 
-function getPlayerInput (){
-    //let input = prompt('Rock, Paper, Scissors?').toLocaleLowerCase();
-    switch (input){
-        case 'rock':
-            return ROCK;
-        case 'paper':
-            return PAPER;
-        case 'scissors':
-            return SCISSORS;
-        default:
-            console.error('Wrong input, check your spelling: ' + input);
+function initGame (){
+    document.querySelectorAll('.game .choice div').forEach((btn) =>{   //Init player input
+        btn.addEventListener('click', (e) => {                          //Setup game flow
+            playerInput = getPlayerInput(e.target);
+            computerInput = getComputerInput();
+
+            descision(playerInput, computerInput);
+
+            updateScore(btn.className , computerInput);
+        })
+    })
+
+    const getPlayerInput = (target) => {
+        console.log(target)
+        switch (target.className){
+            case 'rock':
+                return ROCK;
+            case 'paper':
+                return PAPER;
+            case 'scissors':
+                return SCISSORS;
+            default:
+                console.error('Something wen\'t wrong, please refresh the page!');
+        }
     }
+}
+function updateScore(){
+    document.querySelector('.results .user .move').innerText = playerInput; //Figure choice by css class on pressed button
+    document.querySelector('.results .computer .move').innerText = computerInput;
+
+    document.querySelector('.results .user .counter').innerText = 'Wins:' + playerWins;
+    document.querySelector('.results .computer .counter').innerText = 'Wins:' + computerWins;
+
 }
 
 function getComputerInput() {
-    return Math.random() *3 +1;
+    return parseInt(Math.random() *3 +1);
 }
 
 function descision(playerInput, computerInput){
@@ -38,11 +60,38 @@ function descision(playerInput, computerInput){
     }
     if(playerInput === ROCK){
         if(computerInput === SCISSORS){
+            playerWins++;
             console.log('You won!')
             return;
         }
         else{
+            computerWins++;
             console.log('The computer won :(')
+            return;
+        }
+    }
+    if(playerInput === PAPER){
+        if(computerInput === ROCK){
+            playerWins++;
+            console.log('You won!')
+            return;
+        }
+        else{
+            computerWins++;
+            console.log('The computer won :(')
+            return;
+        }
+    }
+    if(playerInput === SCISSORS){
+        if(computerInput === PAPER){
+            playerWins++;
+            console.log('You won!')
+            return;
+        }
+        else{
+            computerWins++;
+            console.log('The computer won :(')
+            return;
         }
     }
 }
